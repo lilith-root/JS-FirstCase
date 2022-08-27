f2b42b234e4694800f8dffeb0f4107efe5bdc989
lilith-root <isinalc@gmail.com>
2022-08-27 22:39:38 +0300

    initial commit

--- JavaScriptHw.js
+++ JavaScriptHw.js
@@ -1,14 +1,20 @@
-var number = prompt("Enter a number , please :" );
-sum = 0;
+number = prompt("Please enter a number :");
+let sum = 0;
 
 while (number > 0) {
     sum += number % 10;
     number = Math.floor(number / 10);
-}
+
+    function isoddOrEvenish(number){
+
+
         if (sum % 2 == 0) {
             console.log("Even");
 
         }else{
             console.log("Odd");
         } 
+    }
+}
 
+isoddOrEvenish(number)
